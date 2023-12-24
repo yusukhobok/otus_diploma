3e3803b82b569a88d583408567e9c69505d1e7fa
Yuri Sukhobok <khvyus@gmail.com>
2023-12-24 19:31:02 +0500

    add Eigen3 dependency

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,18 @@
 #include <QApplication>
 #include <iostream>
 
+#include <Eigen/Dense>
+using Eigen::MatrixXd;
+
 int main(int argc, char *argv[])
 {
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
+
     Radargram r(10, 20);
     std::cout << r.x << std::endl;
 
