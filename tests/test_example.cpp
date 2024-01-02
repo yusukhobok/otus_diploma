ca23f11b22f7e30c02ee42299d6d8590e2275d66
Yuri Sukhobok <khvyus@gmail.com>
2024-01-02 09:29:21 +0500

    fix

--- tests/test_example.cpp
+++ tests/test_example.cpp
@@ -1,13 +1,9 @@
 #include <gtest/gtest.h>
 #include <iostream>
-#include "../src/domain/radargram.hpp"
 
 TEST(
         ExampleTests,
         given_when_then
 ) {
-    Radargram r(10, 20);
-    std::cout << r.y << std::endl;
 
-    ASSERT_EQ(r.x, 10);
 }
